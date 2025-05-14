64fcb922c586db290bcad7918262af510747532e
theslayermods8110 <e7701080@gmail.com>
2025-05-13 20:03:12 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "voz-en-vivo",
+  "version": "1.0.0",
+  "description": "Servidor WebSocket para sala de voz",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "ws": "^8.0.0"
+  }
+}
